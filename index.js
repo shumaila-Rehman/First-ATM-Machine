67dfd1640a1a1eca32e55b7c28fb5abbfcb6632d
Shumaila-Rehman <pathan369khan@gmail.com>
2024-04-05 21:45:12 +0500

    made some changes

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 #! /usr/bin/env node 
 import inquirer from "inquirer";
+import chalk from "chalk";
 let myBalance = 10000;
 let myPin = 5555;
-console.log("\n\tATM-Machine by shumaila Rehman");
+console.log(chalk.blue("\n\t ATM-Machine by shumaila Rehman"));
 let pinAnswer = await inquirer.prompt([
     {
         name: "Pin",
         type: "number",
-        message: "Enter your pin code"
+        message: chalk.yellow("Enter your pin code")
     }
 ]);
 // 1234 === 5555 =====> false 
 if (pinAnswer.Pin === myPin) {
-    console.log("Your Pin code is correct !");
+    console.log(chalk.green("Your Pin code is correct !"));
     //another question if pin is correct . these all question's code will be write in if condition.
     let operationAns = await inquirer.prompt([{
             name: "operation",
@@ -22,17 +23,43 @@ if (pinAnswer.Pin === myPin) {
         }
     ]);
     if (operationAns.operation === "withdraw amount") {
+        let withdrawAns = await inquirer.prompt([
+            {
+                name: "withdrawMethod",
+                type: "list",
+                message: "Select withdrawal method",
+                choices: ["fast cash", "enter amount"]
+            }
+        ]);
+        if (withdrawAns.withdrawMethod === "fast cash") {
+            let fastCashAns = await inquirer.prompt([
+                {
+                    name: "fastCash",
+                    type: "list",
+                    message: " select amount",
+                    choices: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
+                }
+            ]);
+            if (fastCashAns.fastCash > myBalance) {
+                console.log(chalk.red("Insufficient balance"));
+            }
+            else {
+                myBalance -= fastCashAns.fastCash;
+                console.log(`${fastCashAns.fastCash} withdraw successfully`);
+                console.log(`your remaining balance is : ${myBalance}`);
+            }
+        }
         let amountAns = await inquirer.prompt([
             {
                 name: "amount",
                 type: "number",
-                message: "Enter your amount"
+                message: chalk.bgGreen("Enter your amount")
             }
         ]);
         myBalance -= amountAns.amount;
         console.log(`your remaining balance is: ${myBalance}`);
         if (amountAns.amount > myBalance) {
-            console.log("Insufficient balance");
+            console.log(chalk.red("Insufficient balance"));
         }
     }
     else if (operationAns.operation === "check balance") {
